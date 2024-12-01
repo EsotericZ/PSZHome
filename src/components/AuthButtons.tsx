d70e40d12a4ea7cb4ce0f0a972b1498869cff020
EsotericZ <cjsand03@gmail.com>
2024-12-01 12:00:25 -0700

    added mui and setting up how to login

--- src/components/AuthButtons.tsx
+++ src/components/AuthButtons.tsx
@@ -1,20 +1,29 @@
 import { FC } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from '@mui/material';
 
 const AuthButtons: FC = () => {
   const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 
+  const handleLogin = () => {
+    loginWithRedirect({
+      authorizationParams: {
+        screen_hint: "login",
+      },
+    })
+  }
+
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  }
+
   return (
     <div>
       {!isAuthenticated ? (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <Button onClick={handleLogin}>Log In</Button>
       ) : (
         <>
-          <button
-            onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
-          >
-            Log Out
-          </button>
+          <Button onClick={handleLogout}>Log Out</Button>
           {user && (
             <div>
               <p>Welcome {user.name}</p>
--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactElement } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate } from '@tanstack/react-router';
 import { useAuth0 } from '@auth0/auth0-react';
 
 interface PrivateRouteProps {
