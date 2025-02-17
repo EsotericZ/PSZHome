b1348a9183a39467b20553fbe98960ffb84f5862
unknown <cjsand03@gmail.com>
2025-02-17 06:40:57 -0700

    building blocks for library

---
+++ src/components/library/Backlog.tsx
@@ -0,0 +1,105 @@
+import { FC, useEffect, useState } from 'react';
+import { Box, Typography, useMediaQuery } from '@mui/material';
+
+import { useUserContext } from '../../context/UserContext';
+
+import LoadSymbol from '../shared/LoadSymbol';
+import SearchBar from '../shared/SearchBar';
+import UpdatePSNButton from '../profile/UpdatePSNButton';
+
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+
+const Backlog: FC = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+  const { state } = useUserContext();
+  const apiPrivate = useAxiosPrivate();
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  // const filteredGames = gamesList.filter((game) =>
+  //   game.username.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
+
+  // const fetchData = async (): Promise<{ psnAvatar?: string | null; psnPlus?: boolean }> => {
+  //   if (!state.id) {
+  //     console.warn('UserID Not Available');
+  //     return { psnAvatar: state.psnAvatar, psnPlus: state.psnPlus };
+  //   }
+
+  //   try {
+  //     const friendData = await getAllUserFriends(apiPrivate, state.id);
+  //     setFriendList(friendData);
+
+  //     return { psnAvatar: state.psnAvatar, psnPlus: state.psnPlus };
+  //   } catch (error) {
+  //     console.error(error);
+  //     return { psnAvatar: state.psnAvatar, psnPlus: state.psnPlus };
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  return (
+    state.verified ? (
+      <Box>
+        {loading ? (
+          <LoadSymbol />
+        ) : (
+          <>
+            <Box 
+              sx={{ 
+                display: 'flex', 
+                flexDirection: isMobile ? 'column' : 'row',
+                justifyContent: isMobile ? 'center' : 'space-between',
+                alignItems: 'center', 
+                gap: 2,
+                mb: isMobile ? 1 : 2,
+                maxWidth: '600px',
+                mx: 'auto',
+              }}
+            >
+              <SearchBar 
+                value={searchTerm} 
+                onChange={setSearchTerm} 
+                placeholder='Search Games...'
+              />
+              <Box sx={{ mt: isMobile ? 0 : 2 }}>
+                <UpdatePSNButton
+                  userId={state.id}
+                  // fetchData={fetchData}
+                />
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: 2,
+                justifyContent: 'center',
+                alignItems: 'center',
+                maxWidth: '1000px',
+                margin: '0 auto',
+              }}
+            >
+              {/* {filteredFriends.map((friend, index) => (
+                <Box key={index} sx={{ flex: '1 1 275px', maxWidth: '300px' }}>
+                  <FriendCard friend={friend} />
+                </Box>
+              ))} */}
+            </Box>
+          </>
+        )}
+      </Box>
+    ) : (
+      <Box>
+        <Typography>Verify Your Profile To Use This Feature</Typography>
+      </Box>
+    )
+  )
+}
+
+export default Backlog;
--- src/routes/library/index.lazy.tsx
+++ src/routes/library/index.lazy.tsx
@@ -1,7 +1,14 @@
+import { useContext, useState } from 'react';
 import { createLazyFileRoute } from '@tanstack/react-router';
-import { Box } from '@mui/material';
+import { Box, Tab, Tabs, useMediaQuery } from '@mui/material';
+import { styled } from '@mui/system';
 
+import { ThemeContext } from '../../context/ThemeContext';
 import { useUserContext } from '../../context/UserContext';
+
+import Backlog from '../../components/library/Backlog';
+import PSLibrary from '../../components/library/PSLibrary';
+import Wishlist from '../../components/library/Wishlist';
 
 export const Route = createLazyFileRoute('/library/')({
   component: Library,
@@ -9,16 +16,56 @@ export const Route = createLazyFileRoute('/library/')({
 
 function Library() {
   const { state } = useUserContext();
+  const themeContext = useContext(ThemeContext);
+
+  if (!themeContext) {
+    throw new Error('ThemeContext must be used within a ThemeProvider');
+  }
+
+  const [tab, setTab] = useState<number>(0);
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  const StyledTabs = styled(Tabs)({
+    '& .MuiTabs-indicator': {
+      display: 'none', 
+    },
+  });
+
+  const StyledTab = styled(Tab)({
+    borderRadius: isMobile ? '0px' : '16px',
+    textTransform: 'none',
+    padding: isMobile ? '8px' : '8px 16px',
+    margin: isMobile ? '0 8px' : '0 16px',
+    width: isMobile ? 'auto' : '150px',
+    backgroundColor: isMobile ? 'transparent' : themeContext.theme.iconInactive,
+    color: themeContext.theme.text,
+    '&.Mui-selected': {
+      backgroundColor: isMobile ? 'transparent' : themeContext.theme.primary,
+      color: themeContext.theme.text,
+      borderBottom: isMobile ? `2px solid ${themeContext.theme.primary}` : 'none',
+    },
+  });
 
   return (
-    state.verified ? (
-      <Box>
-        <h3>Library</h3>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 3 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+        <StyledTabs 
+          value={tab} 
+          onChange={(_, newValue) => setTab(newValue)} 
+          centered
+          sx={{ maxWidth: 'fit-content' }}
+        >
+          <StyledTab label={isMobile ? '👤' : 'Library'} />
+          <StyledTab label={isMobile ? '👥' : 'Backlog'} />
+          <StyledTab label={isMobile ? '🎮' : 'Wishlist'} />
+        </StyledTabs>
       </Box>
-    ) : (
-      <Box>
-        <h3>Verify Your Profile To Use This Feature</h3>
+  
+      <Box sx={{ width: '90%', maxWidth: '1200px', mt: 3 }}>
+        {tab === 0 && <PSLibrary />}
+        {tab === 1 && <Backlog />}
+        {tab === 2 && <Wishlist />}
       </Box>
-    )
-  )
+    </Box>
+  );
 }
